7c3f2522f3ed791c091ecb0a999eb4669dfb3790
eclipse021 <junhyeon@naver.com>
2024-11-23 00:54:00 +0900

    build.gradle에 openapi-document 추가 및 Base 기본 config 설정

---
+++ src/main/java/com/example/demo/config/BaseResponseStatus.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BaseResponseStatus {
+
+    //요청 성공
+    Success(true, 200, "ok");
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+}
